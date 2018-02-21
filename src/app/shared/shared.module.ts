b86c9691ab82cace59ce5b8f1769cb156e240182
Jáderson Lopes <jahderson@hotmail.com>
2018-02-20 23:25:37 -0300

    Novos ajustes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,12 @@ import { InputComponent } from './input/input.component'
 import { RadioComponent } from './radio/radio.component'
 import { RatingComponent } from './rating/rating.component'
 
-import {OrderService} from '../order/order.service'
-import {ShoppingCartService} from '../restaurant-detail/shopping-cart/shopping-cart.service'
-import {RestaurantsService} from '../restaurants/restaurants.service';
+import { OrderService } from '../order/order.service'
+import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service'
+import { RestaurantsService } from '../restaurants/restaurants.service';
 import { SnackbarComponent } from './messages/snackbar/snackbar.component'
+
+import { NotificationService } from './messages/notification.service'
 
 @NgModule({
   declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -22,7 +24,7 @@ export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers:[ShoppingCartService, RestaurantsService, OrderService]
+      providers:[ShoppingCartService, RestaurantsService, OrderService, NotificationService]
     }
   }
 }
